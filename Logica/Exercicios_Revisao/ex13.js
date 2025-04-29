37cdb4bb40fe56700af31188f3fa0319b140a29f
Theo Yusuke <theo.fontanin@aluno.senai.br>
2025-04-29 13:23:02 -0300

    correcao

--- Logica/Exercicios_Revisao/ex13.js
+++ Logica/Exercicios_Revisao/ex13.js
@@ -1,15 +1,16 @@
 const prompt = require('prompt-sync')()
 
-let nota1 = Number(prompt('Digite a primeira nota do aluno: '))
-let nota2 = Number(prompt('Digite a segunda nota do aluno: '))
-let nota3 = Number(prompt('Digite a terceira nota do aluno: '))
-let nota4 = Number(prompt('Digite a quarta nota do aluno: '))
-let media = (nota1+nota2+nota3+nota4)/4
-
-if (media < 5){
-    console.log(`Reprovado, media: ${media}`);
-}else if (media > 7){
-    console.log(`Aprovado, media: ${media}`);
-}else{
-    console.log(`Recuperaçao, media: ${media}`);
+function mediaNotas (n1,n2,n3,n4){
+    let media =(n1 +n2 + n3 + n4) / 4
+    if (media < 5){
+        console.log(`Reprovado, media: ${media}`);
+    }else if (media > 7){
+        console.log(`Aprovado, media: ${media}`);
+    }else{
+        console.log(`Recuperaçao, media: ${media}`);
+    }
 }
+
+mediaNotas(7,7,7,6)
+mediaNotas(7,7,7,8)
+mediaNotas(5,5,5,4)
