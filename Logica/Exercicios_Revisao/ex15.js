b33351a743a291ace5566b710b7f16851b6c7d5b
Theo Yusuke <theo.fontanin@aluno.senai.br>
2025-04-29 10:16:35 -0300

    revisao

--- Logica/Exercicios_Revisao/ex15.js
+++ Logica/Exercicios_Revisao/ex15.js
@@ -7,3 +7,4 @@ console.log(`parcelas de 3: R$${divida + (divida * 0.10)} (com juros) e tera de 
 console.log(`parcelas de 6: R$${divida + (divida * 0.15)} (com juros) e tera de pagar R$${((divida + (divida * 0.15)) / 6).toFixed(2)} cada parceça` );
 console.log(`parcelas de 9: R$${divida + (divida * 0.20)} (com juros) e tera de pagar R$${((divida + (divida * 0.20)) / 9).toFixed(2)} cada parceça` );
 console.log(`parcelas de 12: R$${divida + (divida * 0.25)} (com juros) e tera de pagar R$${((divida + (divida * 0.25)) / 12).toFixed(2)} cada parceça`);
+// aihaondb
