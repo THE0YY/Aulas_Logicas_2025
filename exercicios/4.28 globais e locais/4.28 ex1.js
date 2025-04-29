0e9e8ac6f275fa0f6444ea9aac2a38e6e6d0207d
Theo Yusuke <theo.fontanin@aluno.senai.br>
2025-04-29 07:19:55 -0300

    aula

--- exercicios/4.28 globais e locais/4.28 ex1.js
+++ exercicios/4.28 globais e locais/4.28 ex1.js
@@ -8,3 +8,18 @@ vezes()
 vezes()
 vezes()
 vezes()
+
+
+// let contador = 0
+
+// function Contar(){
+//     contador = contador + 1
+//     // contador++
+//     // contador += 1
+// }
+
+// Contar()
+// Contar()
+// Contar()
+
+// console.log(contador);
