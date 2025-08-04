1120e5ea5b04f3e2e8738d6b97cc07183054e6f2
Theo Yusuke <theo.fontanin@aluno.senai.br>
2025-08-04 15:40:22 -0300

    foncionaro

--- omaigoto vsc/coisera/teste.js
+++ omaigoto vsc/coisera/teste.js
@@ -1,6 +1,9 @@
-let a = 0 
-for(let i = 0; i<100; i++){
-    console.log(a);
-    a++
-    
+let a
+let o = 0
+let ran = Math.floor(Math.random() * 100 + 1)
+
+while(a != ran){
+    a = Math.floor(Math.random() * 100 + 1)
+    o++
 }
+console.log(o);
---
+++ zFuncionarios.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')()
+const Funcionarios = [
+      { id: 0, name: "Aurora Almeida", setor: "RH"},
+      { id: 1, name: "Benjamin Souza", setor: "Finanças"},
+      { id: 2, name: "Catarina Dias", setor: "Contabilidade"},
+      { id: 3, name: "Daniel Ribeiro", setor: "Contabilidade"},
+      { id: 4, name: "Elisa Costa", setor: "Vendas"},
+      { id: 5, name: "Felipe Silva", setor: "Vendas"},
+      { id: 6, name: "Gabriela Pereira", setor: "Vendas"}
+    ];
+
+    let id = 6
+    let ac= ''
+while (ac!= 'sair') {
+  ac= prompt('Escolher ação(Tudo,Checar,Adicionar,Sair): ').toLowerCase()
+    if (ac== 'adicionar'){
+      id++
+      Funcionarios.push({id: id, nome: prompt('Digite o nome da pessoa'), setor: prompt("Digite o setor da pessoa")})
+    }else if (ac== 'checar'){
+      id= Number(prompt('Checar funcionario por id: '))
+      console.log(Funcionarios[id]);
+    }else if (ac=="sair"){
+      break
+    }else if (ac=="tudo"){
+      console.log(Funcionarios);
+    }
+}
